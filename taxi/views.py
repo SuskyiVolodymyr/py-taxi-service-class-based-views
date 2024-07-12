8e0490dad324e58f4686544361373329230bfaa9
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-12 13:00:37 +0300

    some fixes

--- taxi/views.py
+++ taxi/views.py
@@ -26,7 +26,7 @@ class ManufacturerListView(generic.ListView):
 class CarListView(generic.ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.select_related()
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarDetailView(generic.DetailView):
@@ -40,4 +40,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.select_related()
+    queryset = Driver.objects.select_related("cars")
