1865442881d4ecd3cfcdff10962b96967db00b8c
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-12 14:03:02 +0300

    fix2

--- taxi/views.py
+++ taxi/views.py
@@ -40,4 +40,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.select_related("cars")
+    queryset = Driver.objects.prefetch_related("cars")
